b2c99984b5bd72fabeabb4a02077ec73ace3c701
eosowelle <eosowelle@gmail.com>
2026-05-19 11:54:02 -0700

    r

---
+++ environ/en06.js
@@ -0,0 +1,12 @@
+
+let environmentTitle = "The Room That Refuses Morning";
+
+let environmentElements = ["curtain", "dust", "warm floor", "half-open door"];
+
+let mainEntity = {
+    name: "The Curtain",
+    type: "soft border",
+    mood: "suspicious",
+    isMoving: true,
+    favoriteElement: environmentElements[1]
+};
